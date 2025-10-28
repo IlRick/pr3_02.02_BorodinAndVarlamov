7d9506a7a021e1abda7810010341d2f36d3342e1
student-a502 <student-a502@STUD-RM11-A502>
2025-10-28 16:58:22 +0500

    Создан метод Send.Borodin

--- pr3_02.02_BorodinAndVarlamov/Program.cs
+++ pr3_02.02_BorodinAndVarlamov/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using Common;
+using Newtonsoft.Json;
 
 namespace pr3_02._02_BorodinAndVarlamov
 {
@@ -12,10 +13,37 @@ namespace pr3_02._02_BorodinAndVarlamov
         public static List<Leaders> Leaders = new List<Leaders>();
         public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
         public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
-        private static intlocalPort=5001;
+        private static int LocalPort=5001;
         public static int MaxSpeed = 15;
         static void Main(string[] args)
         {
+        }
+
+        private static void Send()
+        {
+            foreach(ViewModelUserSettings User in remoteIPAddress)
+            {
+                UdpClient sender =  new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IPAddress),
+                    int.Parse(User.Port) );
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdShake == User.IdShake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователя:  {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n "+ ex.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
         }
     }
 }

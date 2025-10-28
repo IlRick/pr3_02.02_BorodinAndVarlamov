a56ac60657ed056f14418c111f5563c0b3a046f6
student-a502 <student-a502@STUD-RM11-A502>
2025-10-28 16:33:06 +0500

    Модель представления данных.Borodin

---
+++ Common/ViewModelGames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelGames
+    {
+        public Shakes ShakesPlayers= new Shakes();
+        public Shakes.Point Points= new Shakes.Point();
+        public int Top = 0;
+        public int IdShake {  get; set; }
+    }
+}
